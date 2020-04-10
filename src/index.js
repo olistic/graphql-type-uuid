064c1d4d4542ebdef9956c064a8396a71c2f4ed8
Matias Olivera <moliverafreire@gmail.com>
2020-04-10 16:11:00 -0300

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -7,21 +7,21 @@ const GraphQLUUID = new GraphQLScalarType({
   name: 'UUID',
   description:
     'The `UUID` scalar type represents UUID values as specified by [RFC 4122](https://tools.ietf.org/html/rfc4122).',
-  serialize: value => {
+  serialize: (value) => {
     if (!isUUID(value)) {
       throw new TypeError(`UUID cannot represent non-UUID value: ${value}`);
     }
 
     return value.toLowerCase();
   },
-  parseValue: value => {
+  parseValue: (value) => {
     if (!isUUID(value)) {
       throw new TypeError(`UUID cannot represent non-UUID value: ${value}`);
     }
 
     return value.toLowerCase();
   },
-  parseLiteral: ast => {
+  parseLiteral: (ast) => {
     if (ast.kind === Kind.STRING) {
       if (isUUID(ast.value)) {
         return ast.value;
